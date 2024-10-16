7df3f12ed734fb799fc4e894b2e83eeccfe5123a
darkness650 <148747700+darkness650@users.noreply.github.com>
2024-10-17 00:23:35 +0800

    Create Bramble'sApplesVIII.cpp

---
+++ 特色班高级语言程序设计-实验课2-1/08-深入流程控制语句/Bramble'sApplesVIII.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n) {
+		if (n < 0) break;
+		cout << n << "个苹果,真香"<<endl;
+	}
+	cout << "俺滴苹果！还俺苹果！";
+	return 0;
+}
