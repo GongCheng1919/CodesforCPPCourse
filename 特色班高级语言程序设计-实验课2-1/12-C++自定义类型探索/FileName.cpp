81e46ccd77e1e0470f847a1d5093bd038ead9b8a
darkness650 <148747700+darkness650@users.noreply.github.com>
2024-10-18 23:18:42 +0800

    Add files via upload

---
+++ 特色班高级语言程序设计-实验课2-1/12-C++自定义类型探索/FileName.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<fstream>
+using namespace std;
+class student
+{
+public:
+	int chinese;
+	int maths;
+	int English;
+	int sum;
+	string name;
+	student()
+	{
+		cin>> name >> chinese >> maths >> English ;
+		sum = chinese + maths + English;
+	}
+	bool operator==(student& stu) const
+	{
+		if (fabs(chinese - stu.chinese) <= 5 && fabs(maths - stu.maths) <= 5 && fabs(English - stu.English) <= 5 && fabs(sum - stu.sum) <= 10) return true;
+		return false;
+	}
+};
+int main()
+{
+	fstream in("test.txt", ios::out);
+	int n;
+	cin >> n;
+	student* p = new student[n];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+			if (p[i] == p[j])
+				in << p[i].name << " " << p[j].name << endl;
+	}
+	delete []p;
+	return 0;
+}
